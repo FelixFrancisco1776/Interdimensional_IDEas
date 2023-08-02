32a239cf2239bf01d45a6981a8361fa53346cce8
Felix Francisco <Felixfrancisco1776@gmail.com>
2023-08-02 17:14:11 -0400

    added delete note to server.js

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+
 const express = require('express');
 const fs = require('fs');
 const path = require('path');
@@ -10,54 +11,76 @@ app.use(express.json());
 app.use(express.static("public"));
 
 const dataFilePath = path.join(__dirname, "db", "db.json");
-// ↑↑↑↑↑↑↑↑↑↑↑↑ Server Setup ↑↑↑↑↑↑↑↑↑↑↑↑
 
 app.get("/", (req, res) => {
-    const indexPath = path.join(__dirname, "public", "index.html");
-    res.sendFile(indexPath);
-  });
-  
-  app.get("/notes", (req, res) => {
-    const notesPath = path.join(__dirname, "public", "notes.html");
-    res.sendFile(notesPath);
-  });
-  // ↑↑↑↑↑↑↑↑↑↑↑↑ HTML Page Setup ↑↑↑↑↑↑↑↑↑↑↑↑
-  
-app.get("/api/notes", (req, res) => {
-    fs.readFile(dataFilePath, "utf8", (err, data) => {
-      if (err) {
-        console.error(err);
-        return res.status(500).json({ error: "Failed to read notes" });
-      }
-      const notes = JSON.parse(data);
-      res.json(notes);
-    });
-  });
-  // ↑↑↑↑↑↑↑↑↑↑↑↑ read note from the file ↑↑↑↑↑↑↑↑↑↑↑↑
+  const indexPath = path.join(__dirname, "public", "index.html");
+  res.sendFile(indexPath);
+});
 
-  app.post("/api/notes", (req, res) => {
-    const { title, text } = req.body;
-    if (!title || !text) {
-      return res.status(400).json({ error: "Title and text are required" });
+app.get("/notes", (req, res) => {
+  const notesPath = path.join(__dirname, "public", "notes.html");
+  res.sendFile(notesPath);
+});
+
+app.get("/api/notes", (req, res) => {
+  fs.readFile(dataFilePath, "utf8", (err, data) => {
+    if (err) {
+      console.error(err);
+      return res.status(500).json({ error: "Failed to read notes" });
     }
-  
-    fs.readFile(dataFilePath, "utf8", (err, data) => {
+    const notes = JSON.parse(data);
+    res.json(notes);
+  });
+});
+
+app.post("/api/notes", (req, res) => {
+  const { title, text } = req.body;
+  if (!title || !text) {
+    return res.status(400).json({ error: "Title and text are required" });
+  }
+
+  fs.readFile(dataFilePath, "utf8", (err, data) => {
+    if (err) {
+      console.error(err);
+      return res.status(500).json({ error: "Failed to read notes" });
+    }
+
+    const notes = JSON.parse(data);
+    const newNote = { title, text, id: notes.length + 1 };
+    notes.push(newNote);
+
+    fs.writeFile(dataFilePath, JSON.stringify(notes), "utf8", (err) => {
       if (err) {
         console.error(err);
-        return res.status(500).json({ error: "Failed to read notes" });
+        return res.status(500).json({ error: "Failed to save note" });
       }
-  
-      const notes = JSON.parse(data);
-      const newNote = { title, text, id: notes.length + 1 };
-      notes.push(newNote);
-  
-      fs.writeFile(dataFilePath, JSON.stringify(notes), "utf8", (err) => {
-        if (err) {
-          console.error(err);
-          return res.status(500).json({ error: "Failed to save note" });
-        }
-        res.json(newNote);
-      });
+      res.json(newNote);
     });
   });
- // ↑↑↑↑↑↑↑↑↑↑↑↑ add a new note ↑↑↑↑↑↑↑↑↑↑↑↑
+});
+
+app.delete("/api/notes/:id", (req, res) => {
+  const noteId = parseInt(req.params.id);
+
+  fs.readFile(dataFilePath, "utf8", (err, data) => {
+    if (err) {
+      console.error(err);
+      return res.status(500).json({ error: "Failed to read notes" });
+    }
+
+    let notes = JSON.parse(data);
+    notes = notes.filter((note) => note.id !== noteId);
+
+    fs.writeFile(dataFilePath, JSON.stringify(notes), "utf8", (err) => {
+      if (err) {
+        console.error(err);
+        return res.status(500).json({ error: "Failed to delete note" });
+      }
+      res.json({ message: "Note deleted successfully" });
+    });
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
