8bb66274cd18b0c19e5e06b14d5204dbdae951cb
Felix Francisco <Felixfrancisco1776@gmail.com>
2023-08-02 16:04:35 -0400

    added HTML Page setup to server.js

--- server.js
+++ server.js
@@ -11,3 +11,14 @@ app.use(express.static("public"));
 
 const dataFilePath = path.join(__dirname, "db", "db.json");
 // ↑↑↑↑↑↑↑↑↑↑↑↑ Server Setup ↑↑↑↑↑↑↑↑↑↑↑↑
+
+app.get("/", (req, res) => {
+    const indexPath = path.join(__dirname, "public", "index.html");
+    res.sendFile(indexPath);
+  });
+  
+  app.get("/notes", (req, res) => {
+    const notesPath = path.join(__dirname, "public", "notes.html");
+    res.sendFile(notesPath);
+  });
+  // ↑↑↑↑↑↑↑↑↑↑↑↑ HTML Page Setup ↑↑↑↑↑↑↑↑↑↑↑↑
